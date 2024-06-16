c921772a5cced72dadd4b66bbfc75c7214f7ebd9
rodrigojmayer <rodrigojmayer@gmail.com>
2024-06-16 11:41:25 +0200

    type alias challenge employees

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -170,3 +170,29 @@
 //   }
 
 //   setTheme('dark')
+
+
+// ## Challenge
+
+type Employee = { id: number; name: string; department: string }
+type Manager = { id: number; name: string; employees: Employee[] }
+type Staff = Employee | Manager
+
+function printStaffDetails(staff: Staff): void {
+    if('employees' in staff) {
+        console.log(
+            `${staff.name} is a manager of ${staff.employees.length} employees`
+        )
+    } else {
+        console.log(
+            `${staff.name} is an employee in the ${staff.department} department`
+        )
+    }
+}
+
+const alice:Employee = { id: 1, name: 'alice', department: 'Sales'}
+const steve:Employee = { id: 2, name: 'steve', department: 'HR'}
+
+const bob: Manager = { id: 1, name: 'bob', employees: [alice, steve] }
+
+printStaffDetails(bob)
