33ce063e28887b2d6d40f503c82c8350198a08db
rodrigojmayer <rodrigojmayer@gmail.com>
2024-06-26 20:28:19 +0200

    type check by property

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -608,22 +608,44 @@
 // }
 
 
- type ValueType = string | number | boolean;
+//  type ValueType = string | number | boolean;
 
- let value: ValueType;
- const random = Math.random();
- value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true;
+//  let value: ValueType;
+//  const random = Math.random();
+//  value = random < 0.33 ? 'Hello' : random < 0.66 ? 123.456 : true;
 
- function checkValue(value:ValueType):void {
-    if(typeof value === 'string') {
-        console.log(value.toLowerCase());
-        return;
+//  function checkValue(value:ValueType):void {
+//     if(typeof value === 'string') {
+//         console.log(value.toLowerCase());
+//         return;
+//     }
+//     if(typeof value === 'number') {
+//         console.log(value.toFixed(2));
+//         return;
+//     }
+//     console.log(`boolean: ${value}`);
+
+//  }
+//  checkValue(value)
+
+
+
+type Dog = { type: 'dog'; name: string; bark: () => void }
+type Cat = { type: 'cat'; name: string; meow: () => void }
+type Animal = Dog | Cat 
+
+// function makeSound(animal: Animal) {
+//     if (animal.type === 'dog') {
+//         animal.bark()
+//     } else {
+//         animal.meow()
+//     }
+// }
+
+function makeSound(animal:Animal) {
+    if('bark' in animal) {
+        animal.bark()
+    } else {
+        animal.meow()
     }
-    if(typeof value === 'number') {
-        console.log(value.toFixed(2));
-        return;
-    }
-    console.log(`boolean: ${value}`);
-
- }
- checkValue(value)
+}
