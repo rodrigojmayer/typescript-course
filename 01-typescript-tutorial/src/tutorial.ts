8f8da169e5a7186167c39b63c0df9bb46a44a175
rodrigojmayer <rodrigojmayer@gmail.com>
2024-06-28 21:24:50 +0200

    instance of type guard

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -651,13 +651,37 @@
 // }
 
 
-function printLength (str: string | null | undefined) {
+// function printLength (str: string | null | undefined) {
 
-    if(str) {
-        console.log(str.length)
-    } else {
-        console.log('No string provided')
-    }
-}
+//     if(str) {
+//         console.log(str.length)
+//     } else {
+//         console.log('No string provided')
+//     }
+// }
 
-printLength(null)
+// printLength(null)
+
+
+// try {
+//     throw new Error('This is an error')
+// } catch (error) {
+//     if(error instanceof Error) {
+//         console.log(`Caught an error object : ${error.message}`);
+//     } else {
+//         console.log('unknown error...')
+//     }
+// }
+
+// function checkInput(input:Date | string): string {
+//     if(input instanceof Date) {
+//         return input.getFullYear().toString()
+//     }
+//     return input
+// }
+
+// const year = checkInput(new Date())
+// const random = checkInput('2020-05-05')
+
+// console.log(year)
+// console.log(random)
