d2c1444b19d97419894e22c69954d99a95556933
rodrigojmayer <rodrigojmayer@gmail.com>
2024-07-03 23:03:51 +0200

    discriminated unions and exhaustive check using the never type challenge comment

--- 01-typescript-tutorial/src/tutorial.ts
+++ 01-typescript-tutorial/src/tutorial.ts
@@ -723,37 +723,37 @@
 // }
 
 
-type IncrementAction = {
-    type:'increment';
-    amount: number;
-    timestamp: number;
-    user: string;
-};
+// type IncrementAction = {
+//     type:'increment';
+//     amount: number;
+//     timestamp: number;
+//     user: string;
+// };
 
-type DecrementAction = {
-    type:'decrement';
-    amount: number;
-    timestamp: number;
-    user: string;
-};
+// type DecrementAction = {
+//     type:'decrement';
+//     amount: number;
+//     timestamp: number;
+//     user: string;
+// };
 
-type Action = IncrementAction | DecrementAction;
+// type Action = IncrementAction | DecrementAction;
 
-function reducer(state:number, action:Action){
-    switch(action.type){
-        case 'increment':
-            return state + action.amount;
-        case 'decrement':
-            return state - action.amount;
-        default:
-            const unexpectedAction:never = action
-        throw new Error(`Unexpected action : ${unexpectedAction}`)
-    }
-}
+// function reducer(state:number, action:Action){
+//     switch(action.type){
+//         case 'increment':
+//             return state + action.amount;
+//         case 'decrement':
+//             return state - action.amount;
+//         default:
+//             const unexpectedAction:never = action
+//         throw new Error(`Unexpected action : ${unexpectedAction}`)
+//     }
+// }
 
-const newState = reducer(15,{
-    user: "john",
-    type:'increment',
-    amount:5,
-    timestamp:123456
-})
+// const newState = reducer(15,{
+//     user: "john",
+//     type:'increment',
+//     amount:5,
+//     timestamp:123456
+// })
